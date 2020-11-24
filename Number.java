447971f52ccc771b99bcc79f2a3c05f06d281b94
ellakrechmer <ekrechmer20@stuy.edu>
2020-11-23 21:32:10 -0500

    added compareTo and moved equals

--- Number.java
+++ Number.java
@@ -1,3 +1,19 @@
 public abstract class Number  {
-  
+  public abstract double getValue();
+  public int compareTo(Number other){
+    if (this.getValue()>other.getValue()) return 1;
+    else if (this.getValue()<other.getValue()) return -1;
+    else return 0;
+  }
+  public boolean equals(RealNumber other){
+    if (other.getValue()==0 || getValue()==0) {
+      if (other.getValue()==0 && getValue()==0) return true;
+      else return false;
+    }
+    else if (Math.abs((other.getValue()-getValue()))<0.00001*other.getValue()
+             && Math.abs((other.getValue()-getValue()))<0.00001*getValue()) {
+      return true;
+    }
+    return false;
+  }
 }
